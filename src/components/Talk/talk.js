e610efe1ae8a40b1fe4244e4c269b30d0a116456
Asharib Ahmed <asharibashmed143@gmail.com>
2021-11-08 11:17:07 +0500

    update

--- src/components/Talk/talk.js
+++ src/components/Talk/talk.js
@@ -10,6 +10,11 @@ const Talk = () => {
                 <img alt='talk' className="img" width='50' src="https://cdn3.iconfinder.com/data/icons/home-activity-1/512/videoconference-meeting-video-conference-communication-256.png"/>
             </h1>
             <div className='talk'>
+            <div className='talk'>                    
+                    <b>Nov 2021 | </b> 
+                    <a className='link' target="_blank" href='https://youtu.be/kfL-uCmV6Uk'>
+                        Interview at Sindh Madressatul Islam University </a>
+                </div>
                 <div className='talk'>                    
                     <b>Oct 2021 | </b> 
                     <a className='link' target="_blank" href='https://www.linkedin.com/feed/update/urn:li:activity:6851113881838882817/'>
