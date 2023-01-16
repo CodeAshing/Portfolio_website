1e52a9512774e8021de3e7cf5f8c8176b95f7a59
Nomanfayyaz0347 <53466968+Nomanfayyaz0347@users.noreply.github.com>
2023-01-16 18:11:47 +0500

    set

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,5 +1,19 @@
 import React from "react";
 import "./experience.css";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+import 'react-accessible-accordion/dist/fancy-example.css';
+
+
+
+
+
 
 const Experience = () => {
   return (
@@ -9,9 +23,98 @@ const Experience = () => {
         <img width="50" alt="experience" src="img/experience.png" />
       </h1>
 
-      <div className="">
-        {/* <div className="expmain"> */}
-        <div className="expboxe">
+
+
+      
+      <Accordion>
+     
+      <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                       Data Scientist @Star Marketing | Oct 21 - Present
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p>As a Data Science role at Star Marketing where my primary responsibilities include:</p>
+                    <ul>
+                      <li><span>✅</span>Developing a recommendation engine using deep learning technologies to improve customer engagement and product sales.</li>
+                      <li><span>✅</span>Implementing facial recognition in all products to improve user experience and increase security.</li>
+                      <li><span>✅</span>Working with back-end development, designing architecture of databases with SQL and NoSQL, as well as serverless and graph databases.</li>
+                      <li><span>✅</span> Leading the development of an end-to-end SAAS Attendance application, which includes managing the CI/CD pipeline, deploying the application on AWS cloud, and utilizing various AWS services such as facial recognition and SNS.</li>
+                      <li><span>✅</span> Managing and optimizing the performance of the databases and recommending best practices to improve the scalability of the system.</li>
+                      <li><span>✅</span> Utilizing Git for version control and collaboration with team members.</li>
+                      <li><span>✅</span>Writing and executing integration and unit tests to ensure the quality of the code and product.</li>
+                      <li><span>✅</span></li>
+                    </ul>
+                    <p>Throughout my tenure, I have had the opportunity to work with cutting-edge technologies like deep learning, facial recognition and AWS services, which helped me to improve my problem-solving, analytical, and technical skills. Additionally, I had the chance to work with a team of talented engineers, which helped me to improve my teamwork and communication skills. I am excited to continue working in the field of data science and hope to bring my skills to future projects.</p>
+                </AccordionItemPanel>
+            </AccordionItem>
+           <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                          AI Intern @Moyyn | Jan 21 - Feb 21
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p>I had the opportunity to work with a team of developers to build an AI-based architecture for automating the recruitment process. Our goal was to create a system that could take in hundreds of CVs and shortlist the most relevant candidates for a given job.</p>
+                    <p><b>My role and responsibilities included:</b></p>
+                    <ul>
+                    <li><span>✅</span>Assisted in the development of an AI-based architecture for the recruitment system</li>
+                    <li><span>✅</span>Worked with a team to implement natural language processing (NLP) techniques to extract relevant information from CVs</li>
+                    <li><span>✅</span>Utilized machine learning algorithms to analyze and rank candidates based on their qualifications and experience</li>
+                    <li><span>✅</span>Worked with Python and TensorFlow to develop and train the AI models</li>
+                    <li><span>✅</span>Utilized Git for version control and collaboration with team members</li>
+                    <li><span>✅</span>Assisted in the implementation and testing of the system, and worked with the team to optimize its performance</li>
+                    <li><span>✅</span> Communicated progress and results to the management and stakeholders</li>
+                    </ul>
+                    <p>This internship was a great opportunity for me to gain experience in building AI-based systems and working with a team to solve real-world problems. I am excited to continue working in the field of AI and hope to bring my skills to future projects.</p>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                          AI Developer @Alphatrone Technologies | Dec 20 - Jan 21
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p>I had the opportunity to work with a team of developers to build an AI-based architecture for automating the recruitment process. Our goal was to create a system that could take in hundreds of CVs and shortlist the most relevant candidates for a given job.</p>
+                    <p><b>My role and responsibilities included:</b></p>
+                    <ul>
+                    <li><span>✅</span> Led a team of two developers in building a Google Home-type speech recognition project</li>
+                    <li><span>✅</span>Utilized over 10 AI-based models working in parallel to produce a better result than Google Home</li>
+                    <li><span>✅</span>Developed and implemented a project plan to ensure the project was completed on time and within budget</li>
+                    <li><span>✅</span>Collaborated with team members to identify and solve any technical issues that arose during the development process</li>
+                    <li><span>✅</span> Developed and trained AI-based models using TensorFlow and PythonUtilized Google Cloud Platform and AWS to deploy the models and run tests on the system</li>
+                    <li><span>✅</span>Worked with team members to improve the overall performance and accuracy of the speech recognition system.</li>
+                    <li><span>✅</span>Communicated progress and results to the management and stakeholders</li>
+                    <li><span>✅</span> Utilized Git for version control and collaboration with team members</li>
+                    <li><span>✅</span> Worked with natural language processing (NLP) techniques to improve the system's understanding of spoken language.</li>
+                    </ul>
+                    <p>This internship was a great opportunity for me to gain experience in building AI-based systems and working with a team to solve real-world problems. I am excited to continue working in the field of AI and hope to bring my skills to future projects.</p>
+                </AccordionItemPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                    Artificial Intelligence Engineer @AlCodemist | Jan 20 - Oct 21
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p>As an Artificial Intelligence Engineer at AlCodemist, a startup founded by my friend, where I had the opportunity to work with a passionate team of over 10 people. My primary responsibilities included:</p>
+                    <ul>
+                    <li><span>✅</span>Developing a Cheque Verification product using a deep neural network (DNN) pretrained model with Keras to check the validity of a given cheque. </li>
+                    <li><span>✅</span> Building a real-time mask detection model using DNN and other computer vision techniques.</li>
+                    <li><span>✅</span> Utilizing NLP techniques to extract and process text from cheques</li>
+                    <li><span>✅</span>Optimizing the performance of the models and fine-tuning the parameters to improve accuracy. </li>
+                    <li><span>✅</span> Participated in code reviews and collaborated with peers to ensure high-quality code.</li>
+                    <li><span>✅</span> </li>
+                    </ul>
+                    <p>I had the opportunity to work with cutting-edge technologies like deep learning, computer vision and NLP, which helped me to improve my problem-solving, analytical, and technical skills. Additionally, I had the chance to work with a team of passionate and talented engineers, which helped me to improve my teamwork and communication skills. I am excited to continue working in the field of Artificial Intelligence and hope to bring my skills to future projects.</p>
+                </AccordionItemPanel>
+            </AccordionItem>
+       
+        {/* <div className="expboxe">
           <b>Data Scientist @Star Marketing | Oct 21 - Present </b>
           <div className="para">
             As a Data Science role at Star Marketing where my primary responsibilities include:
@@ -23,9 +126,9 @@ const Experience = () => {
             <br /><b />✅ Utilizing Git for version control and collaboration with team members.
             <br /><b />✅ Writing and executing integration and unit tests to ensure the quality of the code and product.
             <br /><br /><b />Throughout my tenure, I have had the opportunity to work with cutting-edge technologies like deep learning, facial recognition and AWS services, which helped me to improve my problem-solving, analytical, and technical skills. Additionally, I had the chance to work with a team of talented engineers, which helped me to improve my teamwork and communication skills. I am excited to continue working in the field of data science and hope to bring my skills to future projects.</div>
-        </div>
+        </div> */}
 
-        <div className="expboxe">
+        {/* <div className="expboxe">
           <b>AI Intern @Moyyn | Jan 21 - Feb 21 </b>
           <div className="para">
             I had the opportunity to work with a team of developers to build an AI-based architecture for automating the recruitment process. Our goal was to create a system that could take in hundreds of CVs and shortlist the most relevant candidates for a given job.
@@ -42,9 +145,9 @@ const Experience = () => {
 
             <br /> <br /><b />This internship was a great opportunity for me to gain experience in building AI-based systems and working with a team to solve real-world problems. I am excited to continue working in the field of AI and hope to bring my skills to future projects.
           </div>
-        </div>
+        </div> */}
 
-        <div className="expboxe">
+        {/* <div className="expboxe">
           <b>AI Developer @Alphatrone Technologies | Dec 20 - Jan 21 </b>
           <div className="para">
             <br /><b />✅ Led a team of two developers in building a Google Home-type speech recognition project
@@ -58,9 +161,9 @@ const Experience = () => {
             <br /><b />✅ Utilized Git for version control and collaboration with team members
             <br /><b />✅ Worked with natural language processing (NLP) techniques to improve the system's understanding of spoken language.
           </div>
-        </div>
+        </div> */}
 
-        <div className="expboxe">
+        {/* <div className="expboxe">
           <b>Artificial Intelligence Engineer @AlCodemist | Jan 20 - Oct 21 </b>
           <div className="para">
             As an Artificial Intelligence Engineer at AlCodemist, a startup founded by my friend, where I had the opportunity to work with a passionate team of over 10 people. My primary responsibilities included:
@@ -72,10 +175,17 @@ const Experience = () => {
             <br /><b />✅ Participated in code reviews and collaborated with peers to ensure high-quality code.
             <br /><br /><b />I had the opportunity to work with cutting-edge technologies like deep learning, computer vision and NLP, which helped me to improve my problem-solving, analytical, and technical skills. Additionally, I had the chance to work with a team of passionate and talented engineers, which helped me to improve my teamwork and communication skills. I am excited to continue working in the field of Artificial Intelligence and hope to bring my skills to future projects.
           </div>
-        </div>
+        </div> */}
 
-      </div>
+     
+
+      </Accordion>
+
+
+
+
     </div>
+    
   );
 };
 export default Experience;
--- src/components/PetProjects/PetProjects.js
+++ src/components/PetProjects/PetProjects.js
@@ -1,5 +1,16 @@
 import React from "react";
 import "./petProjects.css";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+import 'react-accessible-accordion/dist/fancy-example.css';
+
+
 
 const PetProjects = () => {
   return (
@@ -10,7 +21,108 @@ const PetProjects = () => {
       </h1>
 
       {/* <div className="expmain"> */}
-      <div className="">
+
+      <Accordion>
+
+
+          <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                       Enterprise resource planning (ERP)
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>The Enterprise Resource Planning (ERP) project is a comprehensive system designed to manage the wholesale, payment, finance, expenses, and overall process of a real estate business. I was the lead developer on this project and was responsible for the following:</p>
+                    <ul>
+                    <li><span>✅</span> Developing the entire back-end system</li>
+                    <li><span>✅</span> Designing the database architecture</li>
+                    <li><span>✅</span>Managing the CI/CD pipeline </li>
+                    <li><span>✅</span> Leading the front-end team for website and mobile development </li>
+                    <li><span>✅</span>Training employees on how to use the system effectively </li>
+                   
+                    </ul>
+                    <p>This project has been in development for over a year and is now ready to be implemented in the organization. The ERP system will streamline and automate various business processes, providing real-time visibility into key metrics and data, and improve overall efficiency and productivity. The project will ultimately help the real estate business to make better and informed decisions.</p>
+                </AccordionItemPanel>
+            </AccordionItem>
+
+
+            
+          <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        Attendance Application (SAAS)
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>I have worked on a SAAS attendance system application that utilizes QR-based attendance marking and utilizes location data to validate the presence of a person on site.
+</p>
+                    <ul>
+                    <li><span>✅</span>Developed the backend of the SAAS attendance system using various technologies such as Node.js, Express, and MongoDB </li>
+                    <li><span>✅</span>  Implemented the business logic for the QR-based attendance marking and location validation functionality</li>
+                    <li><span>✅</span> Designed and implemented the database architecture for storing and retrieving attendance records</li>
+                    <li><span>✅</span>Conducted research to determine the best technologies for the project, including but not limited to face recognition and location services </li>
+                    <li><span>✅</span>Managed the CI/CD pipeline using tools such as Jenkins and GitHub for continuous integration and deployment on VPC </li>
+                    <li><span>✅</span> Responsible for testing and quality assurance of the application, including functional and non-functional testing </li>
+                    <li><span>✅</span> Utilized face recognition technology for user login </li>
+                    <li><span>✅</span>  Implemented security measures such as encryption and access controls to protect sensitive data</li>
+                    <li><span>✅</span> Monitored and maintained the application's performance and scalability. </li>
+                  
+                    </ul>
+                </AccordionItemPanel>
+            </AccordionItem>
+
+
+            
+          <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                      Finance Portal
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>I have worked on a finance portal for managing the loan application process for real estate properties. My responsibilities included:</p>
+                    <ul>
+                    <li><span>✅</span>Developing the backend of the project using technologies such as Node.js, Express, and MongoDB </li>
+                    <li><span>✅</span> Managing the CI/CD pipeline to ensure continuous integration and deployment</li>
+                    <li><span>✅</span>  Conducting research and development to determine the best technologies and solutions for the project</li>
+                    <li><span>✅</span>  Integrating the portal with a CRM system such as itrix and adding webhooks for seamless communication</li>
+                    <li><span>✅</span> Implementing real-time functionality using web sockets for live updates</li>
+                    <li><span>✅</span>Integrating push notifications using Firebase to keep users informed about the status of their loan applications</li>
+                    <li><span>✅</span>Managing the project and ensuring its smooth operation and performance </li>
+                
+                    </ul>
+                    <p>The loan application process is a critical and complex process, the finance portal I worked on, streamline the process, improve communication and increase efficiency.</p>
+                </AccordionItemPanel>
+            </AccordionItem>
+
+
+
+            
+          <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                       Social Manager
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>I have worked on a project called Social Manager, an application that allows users to post on multiple social media platforms with just one post. My responsibilities included:</p>
+                    <ul>
+                    <li><span>✅</span> Developing the backend of the project using technologies such as Node.js, Express and MongoDB</li>
+                    <li><span>✅</span> Integrating APIs from popular social media platforms like Facebook, YouTube, Instagram, and LinkedIn </li>
+                    <li><span>✅</span> Implementing functionality to upload images and videos to an S3 bucket </li>
+                    <li><span>✅</span>Managing the project and ensuring its smooth operation and performance </li>
+
+                    </ul>
+                    <p>Social Manager is a powerful tool for managing social media presence, with this application users can save time and effort by managing all their social media accounts in one place. The integration with multiple platforms, S3 bucket for media upload and the backend I developed make it a reliable and efficient application.</p>
+                </AccordionItemPanel>
+            </AccordionItem>
+
+
+
+
+
+      </Accordion>
+      {/* <div className="">
         <div className="expboxe">
           <b>Enterprise resource planning (ERP) </b>
           <div className="para">
@@ -71,7 +183,7 @@ const PetProjects = () => {
           </div>
         </div>
 
-      </div>
+      </div> */}
       <br></br>
     </div>
   );
